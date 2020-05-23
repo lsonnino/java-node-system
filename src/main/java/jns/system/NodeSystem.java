167732225d76b897b0c9c573e6fca256a712ea2c
lsonnino <lorenzo@sonnino.com>
2020-05-23 20:18:00 +0200

    Code commented

--- src/main/java/jns/system/NodeSystem.java
+++ src/main/java/jns/system/NodeSystem.java
@@ -4,20 +4,40 @@ import jns.type.TypeSystem;
 
 import java.io.Serializable;
 
+/**
+ * Handles node trees
+ */
 public class NodeSystem implements Serializable {
     private Type[] typeSystem;
     private Node root;
 
+    /**
+     * Default constructor. It has no root nodes and uses the default Type System
+     */
     public NodeSystem(){
         this(TypeSystem.typeSystem);
     }
+    /**
+     * Define a Type System but no root nodes.
+     * Note that every node may not be compatible with every Type Systems
+     * @param typeSystem a Type array defining the type system to use
+     */
     public NodeSystem(Type[] typeSystem){
         this.typeSystem = typeSystem;
     }
 
+    /**
+     * Get the used Type System
+     * @return a Type array defining the current Type System
+     */
     public Type[] getTypeSystem() {
         return typeSystem;
     }
+    /**
+     * Get the Class object corresponding to a specific Type identifier
+     * @param type the type identifier
+     * @return The class object corresponding to the identifier
+     */
     public Class<? extends Data> getType(Enum type){
         for(Type t : typeSystem){
             if(t.is(type))
@@ -27,16 +47,31 @@ public class NodeSystem implements Serializable {
         throw new NodeRuntimeException("Unexpected type " + type.name());
     }
 
+    /**
+     * Set the root node to execute by default
+     * @param root a Node instance with no outputs
+     */
     public void setRoot(Node root) {
         this.root = root;
     }
+    /**
+     * Get the current root node
+     * @return a Node instance if the root node is set, null otherwise
+     */
     public Node getRoot() {
         return root;
     }
 
+    /**
+     * Execute the node tree ending with the specified root node
+     */
     public void run(){
         run(this.root);
     }
+    /**
+     * Execute the node tree ending with a specific root node
+     * @param root a node instance with no output. It will be the end of the tree
+     */
     public void run(Node root){
         if(root == null)
             throw new NodeRuntimeException("No root node");
--- src/main/java/jns/system/OptimizedNode.java
+++ src/main/java/jns/system/OptimizedNode.java
@@ -1,5 +1,9 @@
 package jns.system;
 
+/**
+ * A Node extension that avoid computing twice the same output
+ * If the same output is used twice, only computes once the output at the cost of some extra memory usage
+ */
 public abstract class OptimizedNode extends Node {
     private Data[] loadedOutput;
 
@@ -9,18 +13,35 @@ public abstract class OptimizedNode extends Node {
         loadedOutput = new Data[outputIO.length];
     }
 
+    /**
+     * If the requested output has already been computed, return the answer without computing it again.
+     * Otherwise, compute and remember the answer.
+     * @param requested an IO instance telling which output is requested
+     * @return a Data instance the same type as the node's output containing the answer
+     */
     @Override
     Data<?> run(IO requested) {
-        if(requested.getId() >= 0 && requested.getId() < loadedOutput.length){
+        if(requested.getId() >= 0 && requested.getId() < loadedOutput.length){ // Check the IO id
             Data<?> loaded = loadedOutput[requested.getId()];
-            if(loaded != null){
+            if(loaded != null){ // Check if the output has already been computed
                 return loaded;
+            }
+            else {
+                // Compute the output
+                Data output = super.run(requested);
+                // Remember the output
+                loadedOutput[requested.getId()] = output;
+                return output;
             }
         }
 
+        // Fall back on the default behaviour
         return super.run(requested);
     }
 
+    /**
+     * Reset the node's memory: every output will need to be computed once again if needed
+     */
     @Override
     public void reset() {
         for (int i = 0; i < loadedOutput.length; i++) {
